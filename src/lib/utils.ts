23ddd54472bb8a1216363b1c1bc11d2a094de71f
Gaks3 <dwi66116@gmail.com>
2024-03-05 10:12:58 +0700

    feat: utils function

---
+++ src/lib/utils.ts
@@ -0,0 +1,22 @@
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs))
+}
+
+export const idGenerator = (): string => {
+  return Math.floor(Math.random() * 1001).toString()
+}
+
+export const getDates = (startDate: Date, endDate: Date) => {
+  const dates = []
+  let currentDate = new Date(startDate)
+
+  while (currentDate <= endDate) {
+    dates.push(new Date(currentDate))
+    currentDate.setDate(currentDate.getDate() + 1)
+  }
+
+  return dates
+}
