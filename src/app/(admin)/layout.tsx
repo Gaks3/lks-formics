31506bfedad3f359fe77b529f49dd00987f86eb8
Gaks3 <dwi66116@gmail.com>
2024-03-08 13:25:14 +0700

    fix: remove get current user

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,21 +1,19 @@
 import Logo from '@/components/Logo'
-import { UserButton, currentUser } from '@clerk/nextjs'
-import { notFound } from 'next/navigation'
+import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
 
 export default async function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const user = await currentUser()
-
-  if (user?.publicMetadata.role !== 'admin') return notFound()
-
   return (
     <div className='max-h-screen min-w-full min-h-screen bg-background'>
       <nav className='flex items-center justify-between min-w-full px-6 py-4 border-b border-border'>
         <Logo />
         <div className='flex items-center gap-5'>
+          <Link href={'/'}>Home</Link>
+          <Link href={'/dashboard'}>Dashboard</Link>
           <UserButton afterSignOutUrl='/sign-in' />
         </div>
       </nav>
